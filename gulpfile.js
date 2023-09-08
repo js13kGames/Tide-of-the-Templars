7b23d8c09aae8571f320283956fb10c9defbd2ff
Veronika Milic <veronika.milic@factorial.co>
2023-09-08 23:11:40 +0200

    Revert "WIP (doesnt work)"
    This reverts commit be616d9adf945c86a2c2cc53adfaa448f00dce1e.

--- gulpfile.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const distTasks = require('./tasks/dist.js');
-const watchTasks = require('./tasks/watch.js');
-
-exports.default = distTasks.dist;
-exports.watch = watchTasks.watch;
